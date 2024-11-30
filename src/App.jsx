2e89bbb2072584f8723131c35159e8a7013af5ac
Jeganalagappan <149089899+Jeganalagappan@users.noreply.github.com>
2024-11-30 15:31:42 +0530

    First commit
    First date 30

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import NewUser, { loader as userLoader } from './pages/NewUser';
+import Users from './pages/Users';
+import Home from './pages/Home';
+import './App.css'
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />
+  },
+  {
+    path: 'newuser',
+    element: <NewUser />,
+    loader: userLoader
+  },
+  {
+    path: 'users',
+    element: <Users />,
+    loader: userLoader
+  }
+]);
+
+const App = () => {
+  return (
+    <div>
+      <RouterProvider router={router}/>
+    </div>
+  )
+}
+
+export default App
